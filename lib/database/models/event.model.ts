877766e703cebf361e2359442d63c4f0c872bf51
bonadoni95 <zak.rs9.1995@gmail.com>
2024-01-24 14:58:19 +0700

    implement stripe

--- lib/database/models/event.model.ts
+++ lib/database/models/event.model.ts
@@ -9,7 +9,7 @@ export interface IEvent extends Document {
     imageUrl: string;
     startDateTime: Date;
     endDateTime: Date;
-    price?: string;
+    price: string;
     isFree: boolean;
     url?: string;
     category: { _id: string, name: string };
@@ -24,13 +24,13 @@ const EventSchema = new Schema({
     imageUrl: { type: String, required: true },
     startDateTime: { type: Date, default: Date.now },
     endDateTime: { type: Date, default: Date.now },
-    price: { type: String, },
+    price: { type: String },
     isFree: { type: Boolean, default: false },
     url: { type: String },
     category: { type: Schema.Types.ObjectId, ref: 'Category'},
     organizer: { type: Schema.Types.ObjectId, ref: 'User'},
 })
 
-const Event = models.event || model('Event', EventSchema);
+const Event = models.Event || model('Event', EventSchema);
 
 export default Event;
