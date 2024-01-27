2e4a0bc8866380bea96ffca289f28a4fbaa61026
bonadoni95 <zak.rs9.1995@gmail.com>
2024-01-27 19:15:29 +0700

    fix middleware

--- middleware.ts
+++ middleware.ts
@@ -5,12 +5,12 @@ export default authMiddleware({
         '/',
         '/events/:id',
         '/api/webhook/clerk',
-        '/api/webhook/stripe,',
+        '/api/webhook/stripe',
         '/api/uploadthing'
     ],
     ignoredRoutes: [
         '/api/webhook/clerk',
-        '/api/webhook/stripe,',
+        '/api/webhook/stripe',
         '/api/uploadthing'
     ]
 });
